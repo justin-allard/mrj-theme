0a802bcbd29e3abe5bdc32ef3d4a18257212f9f3
Justin Allard <justinallard@icloud.com>
2025-10-16 15:59:06 -0500

    Refactor plugin update checker initialization
    Updated the way the plugin update checker is required and configured.

--- functions.php
+++ functions.php
@@ -1,11 +1,22 @@
 <?php
-require 'plugin-update-checker/plugin-update-checker.php';
+require get_template_directory() . '/plugin-update-checker/plugin-update-checker.php';
+
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $myUpdateChecker = PucFactory::buildUpdateChecker(
-  'https://github.com/justin-allard/mrj-theme/',
-  __FILE__,
-  'mrjtheme'
+    'https://github.com/justin-allard/mrj-theme/',
+    get_template_directory() . '/functions.php',
+    'mrj-theme'
 );
 
 $myUpdateChecker->setBranch('main');
+
+$api = $myUpdateChecker->getVcsApi();
+if ($api) {
+    $api->enableReleaseAssets();
+}
+
+//add_action('admin_notices', function() use ($myUpdateChecker) {
+//    $state = $myUpdateChecker->getUpdateState();
+//    echo '<pre>'; print_r($state); echo '</pre>';
+//});
