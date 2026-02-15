11bfc238e905cbc060b23bdf01e41114f35b11c8
Justin Allard <justinallard@icloud.com>
2026-02-15 17:12:08 -0600

    Refactor daily ping scheduling and update timeout
    Updated the scheduling of the daily ping event and modified the site URL retrieval method. Increased timeout for the remote POST request.

--- functions.php
+++ functions.php
@@ -30,42 +30,36 @@ function mrj_theme_enqueue_styles() {
 }
 
 
+define( 'MRJ_PING_HOOK', 'mrjtheme_daily_ping' );
 
-// functions.php (theme)
 add_action( 'after_switch_theme', function () {
     if ( ! wp_next_scheduled( MRJ_PING_HOOK ) ) {
-        wp_schedule_event( current_time( 'timestamp' ), 'daily', MRJ_PING_HOOK );
+        wp_schedule_event( time(), 'daily', MRJ_PING_HOOK );
     }
 });
 
 add_action( 'switch_theme', function () {
     wp_clear_scheduled_hook( MRJ_PING_HOOK );
-    wp_clear_scheduled_hook( MRJ_PING_RETRY_HOOK );
 });
 
+add_action( MRJ_PING_HOOK, function () {
 
-// Daily ping callback
-add_action('mrjtheme_daily_ping', function () {
-    // Get the site URL
-    $site_url = get_site_url();
-
-    // Normalize: lowercase, strip protocol, remove trailing slash, strip www.
+    $site_url = home_url();
     $site_url = strtolower(trim($site_url));
     $site_url = preg_replace('#^https?://#', '', $site_url);
     $site_url = rtrim($site_url, '/');
     $site_url = preg_replace('#^www\.#', '', $site_url);
 
-    // Send POST request to API
     wp_remote_post(
         'https://api.mrjtheme.com/api.php',
         [
-            'timeout'  => 5,
+            'timeout'  => 10,
             'blocking' => false,
             'headers'  => [
                 'Content-Type' => 'application/json'
             ],
             'body' => wp_json_encode([
-                'siteURL'       => $site_url,
+                'siteURL'      => $site_url,
                 'ThemeVersion' => wp_get_theme()->get('Version'),
                 'SiteVersion'  => get_bloginfo('version'),
                 'SiteTitle'    => get_bloginfo('name')
